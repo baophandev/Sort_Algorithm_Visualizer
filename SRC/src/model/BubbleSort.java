df5e86f2f540c90c6a89d10e6c94c653fc002e36
Gia Bao dev <baophan.0912@gmail.com>
2024-08-23 22:53:37 +0700

    investigate sort

---
+++ SRC/src/model/BubbleSort.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author GIA BAO
+ */
+public class BubbleSort extends Sort{
+    
+    public BubbleSort(view.VisualPanel visualPanel){
+        super(visualPanel);
+    }
+    
+    public BubbleSort(){
+        super();
+    }
+    
+    @Override
+    public void sort(int[] array, int sortType){
+        
+    }
+}
